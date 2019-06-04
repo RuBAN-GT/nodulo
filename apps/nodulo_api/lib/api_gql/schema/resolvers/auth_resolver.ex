2bef01e36d7bd8eccd4c88c5378f9e59fd0b1578
Dmitry Ruban <dkruban@gmail.com>
2019-06-04 23:36:59 +0300

    Fixed type casting for schema

--- apps/nodulo_api/lib/api_gql/schema/resolvers/auth_resolver.ex
+++ apps/nodulo_api/lib/api_gql/schema/resolvers/auth_resolver.ex
@@ -1,11 +1,12 @@
 defmodule Nodulo.ApiGql.Schema.AuthResolver do
   alias Nodulo.ApiGql.{SignInService, SignUpService}
+  import Nodulo.ApiGql.Schema.AuthType.UserSecrets, only: [from_token: 1]
 
   def sign_in(_parent, %{credentials: params}, _resolution) do
-    {:ok, SignInService.call(params)}
+    {:ok, from_token(SignInService.call(params))}
   end
 
   def sign_up(_parent, %{params: params}, _resolution) do
-    {:ok, SignUpService.call(params)}
+    {:ok, from_token(SignUpService.call(params))}
   end
 end
