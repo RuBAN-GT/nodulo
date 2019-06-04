f539e954ae527832dfbfd0079b49a3fe51d4e4e7
Dmitry Ruban <dkruban@gmail.com>
2019-06-04 23:44:00 +0300

    Soft auth

--- apps/nodulo_api/lib/api_gql/context.ex
+++ apps/nodulo_api/lib/api_gql/context.ex
@@ -6,9 +6,26 @@ defmodule Nodulo.ApiGql.Context do
   """
   @behaviour Plug
 
+  alias Nodulo.ApiGql.Guardian
+  import Plug.Conn
+
   def init(opts), do: opts
 
   def call(conn, _) do
-    Absinthe.Plug.put_options(conn, context: %{})
+    context = build_context(conn)
+    Absinthe.Plug.put_options(conn, context: context)
+  end
+
+  defp build_context(conn) do
+    with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
+         {:ok, current_user, _claims} <- authorize(token) do
+      %{current_user: current_user}
+    else
+      _ -> %{}
+    end
+  end
+
+  defp authorize(token) do
+    Guardian.resource_from_token(token)
   end
 end
